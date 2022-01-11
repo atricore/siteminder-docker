2631289a687b167011fdfc94f8952c116306cbab
Sebastian <sgonzalezoyuela@gmail.com>
2022-01-11 08:10:31 -0500

    dex sample

--- dockerfiles/cadir/14.1.00/container-scripts/setup_psdsa.sh
+++ dockerfiles/cadir/14.1.00/container-scripts/setup_psdsa.sh
@@ -2,3 +2,4 @@
 source /opt/CA/Directory/dxserver/install/.dxprofile 
 /opt/CA/Directory/dxserver/bin/dxserver start psdsa
 ldapadd -h dx -p 7777 -x -f /tmp/cadir_temp/setup.ldif
+ldapadd -h dx -p 7777 -x -f /tmp/cadir_temp/sgonzalez.ldif
